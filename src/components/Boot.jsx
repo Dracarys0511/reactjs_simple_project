8259d37ab78afb78481028fed75cd4ef8e112bb5
Dracarys0511 <65653459+Dracarys0511@users.noreply.github.com>
2021-07-29 01:09:53 +0530

    Add files via upload

---
+++ src/components/Boot.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Boot() {
+  
+  return (
+  <div className="note">
+    <h1>React Js Bootcamp</h1>
+    <p>Learnt a lot. Front End Development is fun, isn't it? </p>
+    </div>
+      );
+}
+
+export default Boot;
